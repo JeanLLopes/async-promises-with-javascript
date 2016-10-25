2191a9a138b98cad73bc8cc3e81412a0c76fcc2d
JeanLLopes <jean.llopes@outlook.com>
2016-10-24 23:58:40 -0200

    Initial Commit

---
+++ index.js
@@ -0,0 +1,20 @@
+let rp = require('request-promise');
+
+let options = {
+    uri:'',
+    headers:{
+        'User-Agent':'Suissa-Chain-Of-Request'
+    },
+    json:true
+}
+
+let url1 = options;
+let url2 = options;
+let url3 = options;
+
+const user = 'suissa';
+url1.uri = 'https://api.github.com/users/' + user;
+
+const cb1 = (response) => {
+    console.log('1) Dados do usuario ' + response.name + ': ', response)
+}    
