fb71c7abdc35755f166d32a6f8124468df8b4d70
JeanLLopes <jean.llopes@outlook.com>
2016-10-25 00:30:00 -0200

    Add new promisses

--- index.js
+++ index.js
@@ -17,8 +17,18 @@ url1.uri = 'https://api.github.com/users/' + user;
 
 const cb1 = (response) => {
     console.log('1) Dados do usuario ' + response.name + ': ', response)
+    url2.uri = response.followers_url;
+    return rp(url2);
+}
+
+const cb2 = (response) => {
+    console.log('2) Lista de Seguidores: ')
+    response.forEach((element,i) =>  {
+        console.log('- ' + element.login)
+    }, this);
 }    
 
 rp(url1)
     .then(cb1)
+    .then(cb2)
     .catch(err => console.log)
